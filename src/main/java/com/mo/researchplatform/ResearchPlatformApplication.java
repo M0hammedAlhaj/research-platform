4ab70aa83bb35fab338d8139df5d0d7247fd4d76
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-11-03 22:25:41 +0300

    init:project

---
+++ src/main/java/com/mo/researchplatform/ResearchPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mo.researchplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mo/researchplatform/ResearchPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mo.researchplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
